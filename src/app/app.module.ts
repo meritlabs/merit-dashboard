0442e23b790ac2a8b5ca4753af2ae0cfc3346614
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-20 17:00:16 +0300

    Generate rank-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './core/components/app-header/app-header.component';
 import { RankViewComponent } from './core/rank-view/rank-view.component';
+import { RankItemComponent } from './core/components/rank/rank-item/rank-item.component';
 
 const appRoutes: Routes = [{ path: '', component: RankViewComponent }];
 
 @NgModule({
-  declarations: [AppComponent, AppHeaderComponent, RankViewComponent],
+  declarations: [AppComponent, AppHeaderComponent, RankViewComponent, RankItemComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
---
+++ src/app/core/components/rank/rank-item/rank-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rank-item',
+  templateUrl: './rank-item.component.html',
+  styleUrls: ['./rank-item.component.scss']
+})
+export class RankItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
