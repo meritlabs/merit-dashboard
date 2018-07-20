aeb42654c75ee17d6d3948a423b5c25f85c6db0a
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-20 16:03:56 +0300

    Generate main component and setup routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './core/app-header/app-header.component';
+import { AppHeaderComponent } from './core/components/app-header/app-header.component';
+import { MainComponent } from './core/main/main.component';
+
+const appRoutes: Routes = [{ path: '', component: MainComponent }];
 
 @NgModule({
-  declarations: [AppComponent, AppHeaderComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, AppHeaderComponent, MainComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
