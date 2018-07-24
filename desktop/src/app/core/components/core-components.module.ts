93eb6673e7a3befd961ecdc37a4d66b3f6423f12
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-24 22:48:52 +0300

    Add icons component

--- desktop/src/app/core/components/core-components.module.ts
+++ desktop/src/app/core/components/core-components.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { AppHeaderComponent } from '@dashboard/desktop/app/core/components/app-sidebar/app-sidebar.component';
 import { RankItemComponent } from '@dashboard/desktop/app/core/components/rank/rank-item/rank-item.component';
 import { LoadingSpinnerComponent } from '@dashboard/desktop/app/core/components/loading-spinner/loading-spinner-small.component';
+import { MeritIconComponent } from '@dashboard/desktop/app/core/components/merit-icon/merit-icon.component';
 
 export function getComponents() {
-  return [AppHeaderComponent, RankItemComponent, LoadingSpinnerComponent];
+  return [AppHeaderComponent, RankItemComponent, LoadingSpinnerComponent, MeritIconComponent];
 }
 
 @NgModule({
---
+++ desktop/src/app/core/components/merit-icon/merit-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'merit-icon',
+  template: '',
+  styleUrls: ['./merit-icon.component.sass'],
+  host: {
+    '[class]': 'name'
+  }
+})
+export class MeritIconComponent {
+  @Input() name: string;
+}
