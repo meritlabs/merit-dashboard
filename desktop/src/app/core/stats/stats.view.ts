36eefd07137d64911cfb16869e7605fdec49eb09
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-25 22:17:26 +0300

    Generate stats view

---
+++ desktop/src/app/core/stats/stats.view.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-view',
+  templateUrl: './stats.view.html',
+  styleUrls: ['./stats.view.sass'],
+})
+export class RankViewComponent implements OnInit {
+  ngOnInit() {}
+}
