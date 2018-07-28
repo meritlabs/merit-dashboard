a748ddecb8bdb4689252d4265f3410f4c1a5ff06
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-28 17:00:53 +0300

    Graph refactoring

--- common/services/network.service.ts
+++ common/services/network.service.ts
@@ -23,6 +23,103 @@ export class NetworkService {
     });
     return network;
   }
+
+  createNodes(arr) {
+    let nodes = {};
+    arr.forEach(function(link) {
+      link.source = nodes[link.source] || (nodes[link.source] = { name: link.label, size: link.weight });
+      link.target = nodes[link.target] || (nodes[link.target] = { name: link.label, size: link.weight });
+    });
+    return nodes;
+  }
+
+  createForce(layout, links, nodes, size, tick) {
+    return layout
+      .force()
+      .nodes(nodes)
+      .links(links)
+      .size(size)
+      .linkDistance(60)
+      .charge(-300)
+      .on('tick', tick)
+      .start();
+  }
+
+  createSvg(select, size) {
+    return select
+      .append('svg')
+      .attr('width', size[0])
+      .attr('height', size[1]);
+  }
+
+  createLink(svg, data) {
+    return svg
+      .selectAll('.link')
+      .data(data)
+      .enter()
+      .append('line')
+      .attr('class', 'link');
+  }
+
+  createNode(svg, nodes) {
+    return svg
+      .selectAll('.node')
+      .data(nodes)
+      .enter()
+      .append('g')
+      .attr('class', 'node');
+  }
+
+  drawMainCircle(node) {
+    node
+      .append('circle')
+      .attr('fill', '#fff')
+      .attr('r', 15);
+    node
+      .append('image')
+      .attr('xlink:href', 'https://www.merit.me/images/favicon.ico')
+      .attr('x', -8)
+      .attr('y', -8)
+      .attr('width', 16)
+      .attr('height', 16);
+    return node;
+  }
+
+  addNodeTitle(node) {
+    node
+      .append('text')
+      .attr('x', 12)
+      .attr('dy', 20)
+      .attr('fill', 'rgb(0, 176, 221)')
+      .attr('stroke', 'rgb(0, 176, 221)')
+      .style('font-size', '11px')
+      .text(function(d) {
+        return d.name;
+      });
+    return node;
+  }
+
+  addPlusButton(node) {
+    console.log(node);
+
+    node
+      .append('circle')
+      .attr('fill', 'rgb(0, 176, 221)')
+      .attr('r', 5)
+      .style('background', 'rgb(0, 176, 221)')
+      .style('transform', 'translateX(-20px)');
+
+    node
+      .append('text')
+      .attr('stroke', '#FFF')
+      .style('transform', 'translate(-23px, 4px)')
+      .style('font-size', '10px')
+      .text(function(d) {
+        return '+';
+      });
+    return node;
+  }
+
   private getRandomIntInclusive() {
     let min = 0;
     let max = 100;
--- desktop/src/app/core/network/network.view.ts
+++ desktop/src/app/core/network/network.view.ts
@@ -16,96 +16,36 @@ export class NetworkViewComponent {
   @ViewChild('canvas') private canvas: ElementRef;
 
   arr = [];
-  async ngAfterViewInit() {
+  async ngOnInit() {
     this.arr = await this.networkService.getNetwork(0);
     this.generateGraph();
   }
 
   generateGraph() {
-    var _this = this;
+    let _this = this;
+    let array = this.arr;
+    let width = window.innerWidth / 1.25;
+    let height = window.innerHeight / 1.25;
+    let layout = D3.layout;
+    let nodes = this.networkService.createNodes(array);
+    let select = D3.select(this.canvas.nativeElement);
+    let size = [width, height];
+    let svg = this.networkService.createSvg(select, size);
+    let values = D3.values(nodes);
+    let force = this.networkService.createForce(layout, array, values, size, tick);
+    let link = this.networkService.createLink(svg, force.links());
+    let node = this.networkService.createNode(svg, force.nodes());
 
-    var nodes = {};
+    node.call(force.drag);
+    node = this.networkService.drawMainCircle(node);
+    node = this.networkService.addNodeTitle(node);
+    node = this.networkService.addPlusButton(node);
 
-    this.arr.forEach(function(link) {
-      link.source = nodes[link.source] || (nodes[link.source] = { name: link.label, size: link.weight });
-      link.target = nodes[link.target] || (nodes[link.target] = { name: link.label, size: link.weight });
-    });
-
-    var width = window.innerWidth / 1.25,
-      height = window.innerHeight / 1.25;
-
-    var force = D3.layout
-      .force()
-      .nodes(D3.values(nodes))
-      .links(this.arr)
-      .size([width, height])
-      .linkDistance(60)
-      .charge(-300)
-      .on('tick', tick)
-      .start();
-
-    var svg = D3.select(this.canvas.nativeElement)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
-
-    var link = svg
-      .selectAll('.link')
-      .data(force.links())
-      .enter()
-      .append('line')
-      .attr('class', 'link');
-
-    var node = svg
-      .selectAll('.node')
-      .data(force.nodes())
-      .enter()
-      .append('g')
-      .attr('class', 'node')
-      .call(force.drag);
-
-    node
-      .append('circle')
-      .attr('fill', '#fff')
-      .attr('r', 15);
-
-    node
-      .append('circle')
-      .attr('fill', 'rgb(0, 176, 221)')
-      .attr('r', 5)
-      .style('background', 'rgb(0, 176, 221)')
-      .style('transform', 'translateX(-20px)');
-    node
-      .append('text')
-      .attr('stroke', '#FFF')
-      .style('transform', 'translate(-23px, 4px)')
-      .style('font-size', '10px')
-      .text(function(d) {
-        return '+';
-      });
-
-    node
-      .append('image')
-      .attr('xlink:href', 'https://www.merit.me/images/favicon.ico')
-      .attr('x', -8)
-      .attr('y', -8)
-      .attr('width', 16)
-      .attr('height', 16);
-
-    node
-      .append('text')
-      .attr('x', 12)
-      .attr('dy', 20)
-      .attr('fill', 'rgb(0, 176, 221)')
-      .attr('stroke', 'rgb(0, 176, 221)')
-      .style('font-size', '11px')
-      .text(function(d) {
-        return d.name;
-      });
     node.on('click', async function() {
-      svg.remove();
-      _this.arr = await _this.networkService.getNetwork(0);
-      _this.generateGraph();
+      // console.log(this);
+      // svg.remove();
+      // _this.arr = await _this.networkService.getNetwork(0);
+      // _this.generateGraph();
     });
 
     function tick() {
