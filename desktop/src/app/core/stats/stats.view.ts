30e31cac27eae6010f0f50c0cc4926af61a0dfb7
Vladimir Gutorov <vlgutv@gmail.com>
2018-08-08 23:02:06 +0300

    Update stats parsing

--- desktop/src/app/core/stats/stats.view.ts
+++ desktop/src/app/core/stats/stats.view.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { Component, OnInit } from '@angular/core';
 import { DashboardAPI_Service } from '@dashboard/common/services/dashboard-api.service';
 import { Store } from '@ngrx/store';
@@ -16,10 +17,14 @@ export class StatsViewComponent implements OnInit {
     blocksToReTarget: 0,
     networkAvgCyclesPS: 0,
     blockTime: 0,
+    lastReTargetBlock: {},
+    reTargetInBlocks: 0,
+    reTargetIn: '',
   };
 
   async ngOnInit() {
     let currentBlockHash = (await this.dashboardAPI.getBlocksInfo(1, 1))[0].hash;
+    let getBlocks = Array.prototype.slice.apply(await this.dashboardAPI.getBlocksInfo(5000, 1));
     let getMiningInfo = Array.prototype.slice.apply(await this.dashboardAPI.getMiningHistoryInfo(30));
     let totalCpS = 0;
     let blockTimeStart = Date.parse(getMiningInfo[0].timestamp);
@@ -29,12 +34,15 @@ export class StatsViewComponent implements OnInit {
       totalCpS += item.networkCyclesPS;
     });
 
+    this.stats.lastReTargetBlock = getBlocks.find(item => item.height % 1440 === 0);
     this.stats.networkCyclesPS = getMiningInfo[0].networkCyclesPS.toFixed(2);
     this.stats.networkAvgCyclesPS = (totalCpS / 30).toFixed(2);
-
     this.stats.currentBlock = await this.dashboardAPI.getBlock(currentBlockHash);
     this.stats.currentBlock.difficulty = this.stats.currentBlock.difficulty.toFixed(3);
     this.stats.blockTime = blockTime;
+    this.stats.reTargetInBlocks = this.stats.currentBlock.height - this.stats.lastReTargetBlock.height;
+    this.stats.reTargetIn = moment(this.stats.lastReTargetBlock.timestamp).format('MM-DD-YY, h:mm A');
+
     console.log(this.stats);
 
     // 1440 - ($currentBlock % 1440);
