092d953e13f3026aac4f38b9551ab288b896c6a6
Vladimir Gutorov <vlgutv@gmail.com>
2018-08-13 22:54:12 +0300

    Wire stats view

--- desktop/src/app/core/stats/stats.view.ts
+++ desktop/src/app/core/stats/stats.view.ts
@@ -1,6 +1,7 @@
 import * as moment from 'moment';
 import { Component, OnInit } from '@angular/core';
 import { DashboardAPI_Service } from '@dashboard/common/services/dashboard-api.service';
+import { Stats } from '@dashboard/common/models/stats';
 
 @Component({
   selector: 'app-stats-view',
@@ -10,23 +11,19 @@ import { DashboardAPI_Service } from '@dashboard/common/services/dashboard-api.s
 export class StatsViewComponent implements OnInit {
   constructor(public dashboardAPI: DashboardAPI_Service) {}
 
-  stats: any = {
-    currentBlock: {},
-    blocksToReTarget: 0,
-    networkAvgCyclesPS: 0,
-    blockTime: 0,
-    lastReTargetBlock: {},
-    reTargetInBlocks: 0,
-    reTargetIn: '',
-    retargetInHours: '',
-    retarget100InHours: '',
-    retargetTimestamp: '',
-    retarget100Timestamp: '',
-    retargetDifficulty: 0,
-    retarget100Difficulty: 0,
-  };
+  stats: Stats;
 
   async ngOnInit() {
-    this.stats = await this.dashboardAPI.getStats();
+    let Stats = (await this.dashboardAPI.getStats()) as Stats;
+    Stats.networkCyclesPS = parseFloat(Stats.networkCyclesPS.toFixed(0));
+    Stats.networkAvgCyclesPS = parseFloat(Stats.networkAvgCyclesPS.toFixed(0));
+    Stats.currentBlock.difficulty = parseFloat(Stats.currentBlock.difficulty.toFixed(0));
+    Stats.lastRetargetBlock.timestamp = `${moment(Stats.lastRetargetBlock.timestamp).format('MMM Do YYYY, hh:mm A')}`;
+    Stats.lastRetargetInBlocks = Stats.currentBlock.height - Stats.lastRetargetBlock.height;
+    Stats.retargetIn = moment(Stats.retargetTimestamp).fromNow();
+    Stats.retargetAt = moment(Stats.retargetTimestamp).format('MMM Do YYYY, hh:mm A');
+    Stats.retargetDifficulty = Stats.retargetDifficulty;
+
+    this.stats = Stats;
   }
 }
