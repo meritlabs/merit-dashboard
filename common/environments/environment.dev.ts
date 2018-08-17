bfb17339908c5916e3aade5f5c4140b427790cc7
Vladimir Gutorov <vlgutv@gmail.com>
2018-08-17 10:47:40 +0300

    Add dev config

---
+++ common/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.dev.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  dashboardApi: 'https://cors-now.herokuapp.com/http://167.99.179.33:8301/',
+  mwsApiUrl: 'https://mws.merit.me/bws/api/v1/',
+  coreAddress: 'MGgAma9epMrSipSm9Y2YjCWGGSt7gJWzM7',
+  nPowTargetTimeSpan: 1140,
+};
+
+export { environment as ENV };
