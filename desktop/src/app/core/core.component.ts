3ced4a3f8a3ac6890c54657194ac4223886c76d8
Vladimir Gutorov <vlgutv@gmail.com>
2018-09-06 11:20:48 +0300

    Change ranking dispatching place

--- desktop/src/app/core/core.component.ts
+++ desktop/src/app/core/core.component.ts
@@ -1,5 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { IAppState } from '@dashboard/common/reducers/app.reducer';
+import { LoadRanks } from '@dashboard/common/actions/rank.action';
+import { IRanks } from '@dashboard/common/models/ranks';
+import { DashboardAPI_Service } from '@dashboard/common/services/dashboard-api.service';
 
 @Component({
   selector: 'core',
@@ -12,11 +17,15 @@ export class CoreComponent {
   bottomMenuItems: any[] = [];
   isNetworkView: boolean;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: Store<IAppState>, public dashboardAPI: DashboardAPI_Service) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.checkCurrentRoute();
     this.router.events.subscribe(event => this.checkCurrentRoute());
+
+    let ranks = (await this.dashboardAPI.getLeaderBoard()) as IRanks;
+    ranks.loading = false;
+    this.store.dispatch(new LoadRanks(ranks));
   }
   checkCurrentRoute() {
     if (this.router.url === '/network' || this.router.url === '/') {
--- desktop/src/app/core/rank/rank.view.ts
+++ desktop/src/app/core/rank/rank.view.ts
@@ -1,22 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { IAppState } from '@dashboard/common/reducers/app.reducer';
-import { LoadRanks } from '@dashboard/common/actions/rank.action';
-import { IRanks } from '@dashboard/common/models/ranks';
-import { DashboardAPI_Service } from '@dashboard/common/services/dashboard-api.service';
 
 @Component({
   selector: 'app-rank-view',
   templateUrl: './rank.view.html',
   styleUrls: ['./rank.view.sass'],
 })
-export class RankViewComponent implements OnInit {
-  constructor(private store: Store<IAppState>, public dashboardAPI: DashboardAPI_Service) {}
+export class RankViewComponent {
+  constructor(private store: Store<IAppState>) {}
 
   ranks$ = this.store.select('ranks');
-  async ngOnInit() {
-    let ranks = (await this.dashboardAPI.getLeaderBoard()) as IRanks;
-    ranks.loading = false;
-    this.store.dispatch(new LoadRanks(ranks));
-  }
 }
